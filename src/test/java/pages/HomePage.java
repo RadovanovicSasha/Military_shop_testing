c9f3e27f86455234c5f6878447abba4cc0f54b1c
Aleksandar Radovanović <radovanovicsasha.rs@gmail.com>
2026-01-20 12:03:05 +0100

    Izmenjene linije code-a 11 i 20 sa komentarima

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -8,7 +8,7 @@ public class HomePage {
     private final WebDriver driver;
 
     // Link "Uloguj se" postoji SAMO kada nisi ulogovan
-    private final By loginLink = By.linkText("Uloguj se");
+    private final By linkZaLogovanje = By.linkText("Uloguj se"); // nova linija - a staro je: private final By loginLink = By.linkText("Uloguj se");
 
     // Link "O nama"
     private final By oNamaLink = By.linkText("O NAMA");
@@ -16,9 +16,8 @@ public class HomePage {
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
-
     public boolean isLoggedIn() {
-        return driver.findElements(loginLink).isEmpty();
+        return driver.findElements(linkZaLogovanje).isEmpty(); // nova linija - a staro je: return driver.findElements(loginLink).isEmpty();
     }
 
     public void openONama() {
